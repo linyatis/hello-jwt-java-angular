2b81c7f93315180c115b5760c01849690593a883
José Filipe Lyra <filipe.lyra@agrofficio.com.br>
2015-09-28 18:13:18 -0300

    Changing implementation to use the wrapper SimpleMessage created.

--- src/main/java/br/com/flyra/examples/simple_java_angular_jwt/servlet/HelloWorldServlet.java
+++ src/main/java/br/com/flyra/examples/simple_java_angular_jwt/servlet/HelloWorldServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.flyra.examples.simple_java_angular_jwt.model.SimpleMessage;
+
+import com.google.gson.Gson;
+
 @WebServlet("/api/hello")
 public class HelloWorldServlet extends HttpServlet {
 
@@ -15,14 +19,16 @@ public class HelloWorldServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 6319303353130827752L;
 
+	private Gson gson = new Gson();
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 		response.setContentType("application/json");
 		response.setCharacterEncoding("utf-8");
 
-		String jsonStr = "{\"msg\": \"Ola Voce Esta logado\"}";
+		SimpleMessage sm = new SimpleMessage("Olá! Você está logado.");
 
-		response.getWriter().print(jsonStr);
+		response.getWriter().print(gson.toJson(sm));
 	}
 }
