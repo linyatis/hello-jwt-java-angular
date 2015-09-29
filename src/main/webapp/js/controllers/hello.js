1bb3a070008014023156f8dcc079425820f30063
José Filipe Lyra <filipe.lyra@agrofficio.com.br>
2015-09-29 13:24:42 -0300

    Changed the get action made to the new "helloService". Some small changes into $mdDialog.

--- src/main/webapp/js/controllers/hello.js
+++ src/main/webapp/js/controllers/hello.js
@@ -2,37 +2,33 @@
 	'use strict';
 	angular.module('login').controller('HelloCtrl', HelloCtrl);
 
-	HelloCtrl.$inject = [ 'logar', '$mdDialog' ];
+	HelloCtrl.$inject = ['helloService', '$mdDialog'];
 
-	function HelloCtrl(logar , $mdDialog) {
+	function HelloCtrl(helloService, $mdDialog) {
 		var vm = this;
 		vm.msg = null;
 		vm.ola = function(ev) {
-			logar.get('/simple-java-angular-jwt/api/hello').then(
-					function(data) {
+			helloService.get()
+				.then(function(result) {
 						$mdDialog.show(
-								$mdDialog.alert()
-							        .parent(angular.element(document.querySelector('#popupContainer')))
-							        .clickOutsideToClose(true)
-							        .title('Bem Vindo Você esta logado"')
-							        .content("Se você esta vendo essa mensagem ok vc esta logado")
-							        .ariaLabel('If i got this menssagem, Ok you are loged')
-							        .ok(data.statusText)
-							        .targetEvent(ev)
-							    );
+							$mdDialog.alert()
+						        .title('Acesso autorizado')
+						        .content(result.data.msg)
+						        .ok("Ok")
+						        .targetEvent(ev)
+						    );
 						vm.img = img();
-					}, function(data) {
-						$mdDialog.show(
-								$mdDialog.alert()
-							        .parent(angular.element(document.querySelector('#popupContainer')))
-							        .clickOutsideToClose(true)
-							        .title('Existe algo errando em seu login')
-							        .content("Para logar configa seus dados")
-							        .ariaLabel('Any is missing, try check your creditals')
-							        .ok(data.statusText)
-							        .targetEvent(ev)
-							    );
-					});
+					})
+				.catch(function(result) {
+					$mdDialog.show(
+						$mdDialog.alert()
+					        .title('Ops! Sem autorização... =(')
+					        .content(result.data.msg)
+					        .ok("Ok")
+					        .targetEvent(ev)
+					    );
+					vm.img = null;
+				});
 		};
 		function img(){
 			return 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSKtI0nP3nJI9i4isJBUVTqOuGdOgbh7xRYVHesh2iEux8lCmRA';
